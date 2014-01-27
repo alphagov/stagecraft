6876d8f51225e141ea051301485a0a1a0912244d
Paul M Furley <paul@paulfurley.com>
2014-01-27 13:26:03 +0000

    Create a blank django app ``datasets``
    From command: ``django-admin.py startapp datasets``

--- stagecraft/stagecraft/settings/common.py
+++ stagecraft/stagecraft/settings/common.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'stagecraft.apps.datasets',
 )
 
 MIDDLEWARE_CLASSES = (
