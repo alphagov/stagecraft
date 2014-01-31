03e123db407f67690380e2e6106e3baade1a6595
Paul M Furley <paul@paulfurley.com>
2014-01-31 16:36:33 +0000

    Add DataGroup model, migration, tests
    See https://www.pivotaltracker.com/story/show/64224278

--- stagecraft/apps/datasets/models/__init__.py
+++ stagecraft/apps/datasets/models/__init__.py
@@ -1,1 +1,2 @@
 from stagecraft.apps.datasets.models.data_type import DataType
+from stagecraft.apps.datasets.models.data_group import DataGroup
---
+++ stagecraft/apps/datasets/tests/models/test_data_group.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+# See https://docs.djangoproject.com/en/1.6/topics/testing/tools/
+
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+from django.core.exceptions import ValidationError
+
+from nose.tools import assert_raises
+
+from stagecraft.apps.datasets.models import DataGroup
+
+
+class DataGroupTestCase(TestCase):
+    def test_data_group_name_must_be_unique(self):
+        a = DataGroup.objects.create(name='foo')
+        a.validate_unique()
+
+        b = DataGroup(name='foo')
+        assert_raises(ValidationError, lambda: b.validate_unique())
+
+
+def test_character_allowed_in_name():
+    for character in 'a1_-':
+        yield _assert_name_is_valid, character * 10
+
+
+def test_character_not_allowed_in_name():
+    for character in '!"£$%^&*()=+':
+        yield _assert_name_not_valid, character * 10
+
+
+def _assert_name_is_valid(name):
+    DataGroup(name=name).full_clean()
+
+
+def _assert_name_not_valid(name):
+    assert_raises(
+        ValidationError,
+        lambda: DataGroup(name=name).full_clean())
