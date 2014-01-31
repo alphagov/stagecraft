2ae38d97e88fefaf313c07977c0dfc9a4b0115fd
Paul M Furley <paul@paulfurley.com>
2014-01-31 15:45:46 +0000

    Add tests for DataType class.
    Note that these require django-nose instead of the stock django test
    runner, due to the use of generator tests (see nose docs)

---
+++ stagecraft/apps/datasets/tests/models/test_data_type.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+# See https://docs.djangoproject.com/en/1.6/topics/testing/tools/
+
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+from django.core.exceptions import ValidationError
+
+from nose.tools import assert_raises
+
+from stagecraft.apps.datasets.models import DataType
+
+
+class DataTypeTestCase(TestCase):
+    def test_data_type_name_must_be_unique(self):
+        a = DataType.objects.create(name='foo')
+        a.validate_unique()
+
+        b = DataType(name='foo')
+        assert_raises(ValidationError, lambda: b.validate_unique())
+
+
+def test_character_allowed_in_name():
+    for character in 'a1_-':
+        yield _assert_name_is_valid, character * 10
+
+
+def test_character_not_allowed_in_name():
+    for character in '!"£$%^&*()=+':
+        yield _assert_name_not_valid, character * 10
+
+
+def _assert_name_is_valid(name):
+    DataType(name=name).full_clean()
+
+
+def _assert_name_not_valid(name):
+    assert_raises(
+        ValidationError,
+        lambda: DataType(name=name).full_clean())
