580cd1ecdb5949414b513b70bd74326d88682b42
Jacob Ashdown <jcbashdown@gmail.com>
2014-02-03 16:49:30 +0000

    Working pep8
    also need coverage and display test results with display result function

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
-
 #!/bin/bash
+
+set -o pipefail
+
 export DJANGO_SETTINGS_MODULE="stagecraft.settings.development"
 
 if [ -d "venv" ]; then
@@ -7,6 +9,29 @@ if [ -d "venv" ]; then
 fi
 
 python manage.py test
+
+function display_result {
+    RESULT=$1
+    EXIT_STATUS=$2
+    TEST=$3
+
+    if [ $RESULT -ne 0 ]; then
+      echo
+      echo -e "\033[31m$TEST failed\033[0m"
+      echo
+      exit $EXIT_STATUS
+    else
+      echo
+      echo -e "\033[32m$TEST passed\033[0m"
+      echo
+    fi
+}
+
+basedir=$(dirname $0)
+outdir="$basedir/out"
+
+rm -f "$outdir/*"
+mkdir -p "$outdir"
 
 # run style check
 $basedir/pep-it.sh | tee "$outdir/pep8.out"
