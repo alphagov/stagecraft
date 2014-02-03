9af1677f024906302b86d9471a807dc2170a2293
Jacob Ashdown <jcbashdown@gmail.com>
2014-02-03 14:19:13 +0000

    pep8 copied from pingdom and run_tests added

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE="stagecraft.settings.development"
 
@@ -5,5 +6,8 @@ if [ -d "venv" ]; then
     source venv/bin/activate
 fi
 
-python manage.py runserver 0.0.0.0:8080
+python manage.py test
 
+# run style check
+$basedir/pep-it.sh | tee "$outdir/pep8.out"
+display_result $? 3 "Code style check"
