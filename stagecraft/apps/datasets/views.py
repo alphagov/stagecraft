424562c8ba636cbec8696188d2e403b2d52cecde
Nick Gravgaard <nick@beautifulcode.net>
2014-02-10 16:31:17 +0000

    Improving error JSON in data-set views

--- stagecraft/apps/datasets/views.py
+++ stagecraft/apps/datasets/views.py
@@ -1,7 +1,8 @@
 import json
 
 from django.core import serializers
-from django.http import HttpResponse, HttpResponseBadRequest, Http404
+from django.http import (HttpResponse, HttpResponseBadRequest,
+                         HttpResponseNotFound)
 from django.shortcuts import render
 
 from stagecraft.apps.datasets.models import DataSet
@@ -11,7 +12,9 @@ def detail(request, name):
     try:
         data_set = DataSet.objects.get(name=name)
     except DataSet.DoesNotExist:
-        raise Http404
+        error = {'status': 'error',
+                 'message': "No Data Set named '{}' exists".format(name)}
+        return HttpResponseNotFound(json.dumps(error))
 
     serialized = serializers.serialize('python', [data_set])
     result = serialized[0]['fields']
@@ -29,7 +32,12 @@ def list(request, data_group=None, data_type=None):
 
     # 400 if any query string keys were not in allowed set
     if not set(request.GET).issubset(key_map):
-        return HttpResponseBadRequest()
+        unrecognised = set(request.GET).difference(key_map)
+        unrecognised_text = ', '.join("'{}'".format(i) for i in unrecognised)
+        error = {'status': 'error',
+                 'message': 'Unrecognised parameter(s) ({}) were provided'
+                            .format(str(unrecognised_text))}
+        return HttpResponseBadRequest(json.dumps(error))
 
     # get allowed filter parameters
     kwargs = {key_map[k]: v for k, v in request.GET.items() if k in key_map}
