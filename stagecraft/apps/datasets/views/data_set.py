0004720988ddcd50828fbb8c1ae92a3a996d92ea
Paul M Furley <paul@paulfurley.com>
2014-02-13 16:50:04 +0000

    Replace serialize library, do it manually
    After discussion it turns out it's better to use json.dumps on a
    manually-built OrderedDict based on fields in the model.

--- stagecraft/apps/datasets/views/data_set.py
+++ stagecraft/apps/datasets/views/data_set.py
@@ -2,9 +2,7 @@ import json
 
 from django.http import (HttpResponse, HttpResponseBadRequest,
                          HttpResponseNotFound)
-from django.shortcuts import render
 
-from stagecraft.libs.serialize import serialize_to_json
 from stagecraft.apps.datasets.models import DataSet
 
 
@@ -16,7 +14,7 @@ def detail(request, name):
                  'message': "No Data Set named '{}' exists".format(name)}
         return HttpResponseNotFound(json.dumps(error))
 
-    json_str = serialize_to_json(data_set)
+    json_str = json.dumps(data_set.serialize())
 
     return HttpResponse(json_str, content_type='application/json')
 
@@ -43,6 +41,6 @@ def list(request, data_group=None, data_type=None):
 
     filter_kwargs = get_filter_kwargs(key_map, request.GET.items())
     data_sets = DataSet.objects.filter(**filter_kwargs)
-    json_str = serialize_to_json(data_sets)
+    json_str = json.dumps([ds.serialize() for ds in data_sets])
 
     return HttpResponse(json_str, content_type='application/json')
