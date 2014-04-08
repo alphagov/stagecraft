3ac55351ec6510d1fe8567f1801755688b36465c
Jacob Ashdown <jcbashdown@gmail.com>
2014-04-08 13:29:40 +0100

    Test outline for mass updating bearer tokens

---
+++ stagecraft/libs/mass_update/data_set_mass_update.py
@@ -0,0 +1,4 @@
+class DataSetMassUpdate():
+    @classmethod
+    def update_bearer_token_for_data_type_or_group_name(cls, query, new_token):
+        pass
---
+++ stagecraft/libs/mass_update/test_data_set_mass_update.py
@@ -0,0 +1,124 @@
+import mock
+from stagecraft.apps.datasets.models import DataGroup, DataSet, DataType
+from stagecraft.libs.backdrop_client import (
+    disable_backdrop_connection)
+from django.test import TestCase
+from stagecraft.libs.mass_update import DataSetMassUpdate
+from nose.tools import assert_equal
+
+
+class TestDataSetMassUpdate(TestCase):
+    @classmethod
+    @disable_backdrop_connection
+    def setUpClass(cls):
+        cls.data_group1 = DataGroup.objects.create(name='datagroup1')
+        cls.data_group2 = DataGroup.objects.create(name='datagroup2')
+        cls.data_type1 = DataType.objects.create(name='datatype1')
+        cls.data_type2 = DataType.objects.create(name='datatype2')
+
+        cls.dataset_a = DataSet.objects.create(
+            name='foo',
+            data_group=cls.data_group1,
+            bearer_token="abc123",
+            data_type=cls.data_type1)
+
+        cls.dataset_b = DataSet.objects.create(
+            name='bar',
+            data_group=cls.data_group2,
+            bearer_token="def456",
+            data_type=cls.data_type1)
+
+        cls.dataset_c = DataSet.objects.create(
+            name='baz',
+            data_group=cls.data_group2,
+            bearer_token="999999",
+            data_type=cls.data_type2)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+        #cannot delete yet - which is getting tricky
+        #cls.dataset_a.delete()
+        #cls.dataset_b.delete()
+        #cls.dataset_c.delete()
+        #cls.data_group1.delete()
+        #cls.data_group2.delete()
+        #cls.data_type1.delete()
+        #cls.data_type2.delete()
+
+    @mock.patch('stagecraft.apps.datasets.models.data_set.purge')
+    @mock.patch('stagecraft.apps.datasets.models.data_set.'
+                'get_data_set_path_queries')
+    @disable_backdrop_connection
+    def test_update_bearer_token_by_date_type(
+            self,
+            mock_get_path_queries,
+            mock_purge):
+
+        new_bearer_token = "ghi789"
+        mock_get_path_queries.return_value = ['/some_url']
+
+        query = {u'data_type': self.data_type1.name}
+        DataSetMassUpdate.update_bearer_token_for_data_type_or_group_name(
+            query, new_bearer_token)
+
+        dataset_a = DataSet.objects.get(id=self.dataset_a.id)
+        dataset_b = DataSet.objects.get(id=self.dataset_b.id)
+        dataset_c = DataSet.objects.get(id=self.dataset_c.id)
+
+        assert_equal(dataset_a.bearer_token, new_bearer_token)
+        assert_equal(dataset_b.bearer_token, new_bearer_token)
+        assert_equal(dataset_c.bearer_token == new_bearer_token, False)
+        mock_purge.assert_called_twice_with(['/some_url'])
+
+    @mock.patch('stagecraft.apps.datasets.models.data_set.purge')
+    @mock.patch('stagecraft.apps.datasets.models.data_set.'
+                'get_data_set_path_queries')
+    @disable_backdrop_connection
+    def test_update_bearer_token_by_data_group(
+            self,
+            mock_get_path_queries,
+            mock_purge):
+
+        new_bearer_token = "ghi789"
+        mock_get_path_queries.return_value = ['/some_url']
+
+        query = {u'data_group': self.data_group2.name}
+        DataSetMassUpdate.update_bearer_token_for_data_type_or_group_name(
+            query, new_bearer_token)
+
+        dataset_a = DataSet.objects.get(id=self.dataset_a.id)
+        dataset_b = DataSet.objects.get(id=self.dataset_b.id)
+        dataset_c = DataSet.objects.get(id=self.dataset_c.id)
+
+        assert_equal(dataset_a.bearer_token == new_bearer_token, False)
+        assert_equal(dataset_b.bearer_token, new_bearer_token)
+        assert_equal(dataset_c.bearer_token, new_bearer_token)
+        mock_purge.assert_called_twice_with(['/some_url'])
+
+    @mock.patch('stagecraft.apps.datasets.models.data_set.purge')
+    @mock.patch('stagecraft.apps.datasets.models.data_set.'
+                'get_data_set_path_queries')
+    @disable_backdrop_connection
+    def test_update_bearer_token_by_data_group_and_data_type(
+            self,
+            mock_get_path_queries,
+            mock_purge):
+
+        new_bearer_token = "ghi789"
+        mock_get_path_queries.return_value = ['/some_url']
+
+        query = {
+            u'data_type': self.data_type1.name,
+            u'data_group': self.data_group2.name}
+        DataSetMassUpdate.update_bearer_token_for_data_type_or_group_name(
+            query, new_bearer_token)
+
+        dataset_a = DataSet.objects.get(id=self.dataset_a.id)
+        dataset_b = DataSet.objects.get(id=self.dataset_b.id)
+        dataset_c = DataSet.objects.get(id=self.dataset_c.id)
+
+        assert_equal(dataset_a.bearer_token == new_bearer_token, False)
+        assert_equal(dataset_b.bearer_token, new_bearer_token)
+        assert_equal(dataset_c.bearer_token == new_bearer_token, False)
+        mock_purge.assert_called_three_times_with(['/some_url'])
