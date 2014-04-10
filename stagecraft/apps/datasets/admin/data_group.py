6913bd431c5571693aa0392245282d7476916eed
Anna Powell-Smith <annapowellsmith@gmail.com>
2014-04-10 17:48:08 +0100

    Add search fields to admin list displays.
    To make it easier to find rows quickly, add a search field to the
    admin list display.

--- stagecraft/apps/datasets/admin/data_group.py
+++ stagecraft/apps/datasets/admin/data_group.py
@@ -5,6 +5,6 @@ from stagecraft.apps.datasets.models.data_group import DataGroup
 
 
 class DataGroupAdmin(reversion.VersionAdmin):
-    pass
+    search_fields = ['name']
 
 admin.site.register(DataGroup, DataGroupAdmin)
