b2b04987766faeac09b5e369971cd9a2a2554b91
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2014-05-12 17:23:54 +0100

    Add url route for user lookup
    - new matcher based on looking up by email in url
    - implies a split in views in the import

--- stagecraft/urls.py
+++ stagecraft/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 
-from stagecraft.apps.datasets import views as datasets_views
+from stagecraft.apps.datasets.views import data_set as datasets_views
+from stagecraft.apps.datasets.views import backdrop_user as backdrop_user_views
 from stagecraft.libs.status import views as status_views
 
 admin.autodiscover()
@@ -19,6 +20,7 @@ urlpatterns = patterns(
                                               permanent=True,
                                               query_string=True)),
     url(r'^data-sets/(?P<name>[\w-]+)$', datasets_views.detail),
+    url(r'^users/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})$', backdrop_user_views.detail),
     url(r'^_status/data-sets$', datasets_views.health_check),
 
     url(r'^_status$', status_views.status),
