5cd459485fee2611eb96229b0a600b3a21f4fc13
Paul M Furley <paul@paulfurley.com>
2014-05-15 11:11:05 +0100

    Fix typo in BackdropUser admin model

--- stagecraft/apps/datasets/admin/backdrop_user.py
+++ stagecraft/apps/datasets/admin/backdrop_user.py
@@ -14,7 +14,7 @@ class DataSetInline(admin.StackedInline):
 
 class BackdropUserAdmin(reversion.VersionAdmin):
     search_fields = ['email']
-    list_display = ('email', 'numer_of_datasets_user_has_access_to',)
+    list_display = ('email', 'number_of_datasets_user_has_access_to',)
     list_per_page = 30
     filter_horizontal = ('data_sets',)
 
@@ -23,10 +23,10 @@ class BackdropUserAdmin(reversion.VersionAdmin):
             dataset_count=models.Count('data_sets')
         )
 
-    def numer_of_datasets_user_has_access_to(self, obj):
+    def number_of_datasets_user_has_access_to(self, obj):
         return obj.dataset_count
 
-    numer_of_datasets_user_has_access_to.admin_order_field = 'dataset_count'
+    number_of_datasets_user_has_access_to.admin_order_field = 'dataset_count'
 
 
 admin.site.register(BackdropUser, BackdropUserAdmin)
