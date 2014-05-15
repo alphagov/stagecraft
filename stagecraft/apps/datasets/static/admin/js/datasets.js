fc4a238375bd4e5a185dc2ec988ae11043b04487
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2014-05-15 12:55:36 +0100

    Add data_set_name preview to data_set admin form
    - Grabs the values of data_group and data_type bound to a change event on those drop downs
    - populates a <p> tag to show a preview
    - BUG / TODO: Doesn't update the preview when a new data group/type is added to the form. I think this is because whatever django magic that adds this new select option in is swallowing the event, so the name preview function does not trigger

--- stagecraft/apps/datasets/static/admin/js/datasets.js
+++ stagecraft/apps/datasets/static/admin/js/datasets.js
@@ -1,35 +1,56 @@
 (function( $ ) {
 
-  function generateRandomString(length) {
-    var CHARS = "abcdefghjkmnpqrstuvwxyz23456789",
-        output = "";
-    while (output.length < length) {
-      output += CHARS[Math.floor(Math.random() * CHARS.length)];
+    function generate_data_set_name(data_group_name, data_type_name, $data_set_name_selector) {
+      var data_set_name = [data_group_name, data_type_name].join('_').replace('-','_','g');
+
+      if(!$data_set_name_selector.length){
+        $('<p id=id_data_set>'+data_set_name+'</p>').insertAfter('.field-name label');
+      }
+      $data_set_name_selector.html(data_set_name);
     }
-    return output;
-  }
 
-  function addGenerateTokenLink() {
-    var $token_field = $("#id_bearer_token"),
-  $anchor = $('<a href="#">generate token</a>');
-
-    $token_field.after($anchor);
-
-    $anchor.on("click", function(event){
-      event.preventDefault();
-
-      var answer = true;
-      if ($token_field.val().length > 0) {
-        answer = confirm("The bearer token field is not empty, are you sure you want to regenerate it?");
+    function generateRandomString(length) {
+      var CHARS = "abcdefghjkmnpqrstuvwxyz23456789",
+          output = "";
+      while (output.length < length) {
+        output += CHARS[Math.floor(Math.random() * CHARS.length)];
       }
-      if (answer) {
-  $token_field.val(generateRandomString(64));
+      return output;
+    }
+
+    function addGenerateTokenLink() {
+      var $token_field = $("#id_bearer_token"),
+          $anchor = $('<a href="#">generate token</a>');
+
+      $token_field.after($anchor);
+
+      $anchor.on("click", function(event){
+        event.preventDefault();
+
+        var answer = true;
+        if ($token_field.val().length > 0) {
+          answer = confirm("The bearer token field is not empty, are you sure you want to regenerate it?");
+        }
+        if (answer) {
+            $token_field.val(generateRandomString(64));
+        }
+
+      });
+
+    }
+
+    $(addGenerateTokenLink);
+
+  $(function() {
+
+    $("#id_data_group, #id_data_type").on('change', function(){
+        generate_data_set_name(
+          $("#id_data_group").find("option:selected").text(),
+          $("#id_data_type").find("option:selected").text(),
+          $("#id_data_set")
+        );
       }
-
-    });
-
-  }
-
-  $(addGenerateTokenLink);
+    );
+  });
 
 })( django.jQuery );
