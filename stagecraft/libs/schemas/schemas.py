0f9432482b16de371c14332fb447acd3bc9efec3
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2014-06-17 18:29:12 +0100

    Add a simple switch to pull back various different kinds of schema
    - Obviously not a sustainable model for schema sniffing, but okay for two types?
    - There must be a better way to do inheritance than this, but it's good to have something to test against

--- stagecraft/libs/schemas/schemas.py
+++ stagecraft/libs/schemas/schemas.py
@@ -3,11 +3,18 @@ from json import loads as json_loads
 from os import path
 
 
-def get_schema():
+def get_schema(self):
+
+    schema_path = 'stagecraft/apps/datasets/schemas/timestamp.json'
+
+    if(self.data_type == "monitoring"):
+        schema_path = 'stagecraft/apps/datasets/schemas/monitoring.json'
+
     schema_root = path.join(
         settings.BASE_DIR,
-        'stagecraft/apps/datasets/schemas/timestamp.json'
+        schema_path
     )
+
     with open(schema_root) as f:
         schema = json_loads(f.read())
 
