920541db753de894f7d9a52346fce4e20f23812c
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2014-06-19 13:17:45 +0100

    Return the schemas folder, rather than writing out the list

--- stagecraft/libs/schemas/schemas.py
+++ stagecraft/libs/schemas/schemas.py
@@ -3,43 +3,45 @@ logger = logging.getLogger(__name__)
 
 from django.conf import settings
 from json import loads as json_loads
-from os import path
+from os import path, listdir
 
 SCHEMA_PATH = 'stagecraft/apps/datasets/schemas/'
 schema_root = path.join(
     settings.BASE_DIR,
     SCHEMA_PATH
 )
-DEFINED_SCHEMAS = {
-    'data_types': ['monitoring']
-}
 
 
-def load_json_schema(schema_name):
+def get_defined_schemas(name=None):
+    fetch = path.join(schema_root, name)
+    return [s_dir.replace(".json", "") for s_dir in listdir(fetch)]
 
-    if not ".json" in schema_name:
-        schema_name = schema_name + ".json"
 
-    schema_path = path.join(schema_root, schema_name)
+def load_json_schema(schema_path):
+
+    if not ".json" in schema_path:
+        schema_path = schema_path + ".json"
+
+    schema_path = path.join(schema_root, schema_path)
 
     with open(schema_path) as schema_file:
         return json_loads(schema_file.read())
 
 
-def get_schema(self):
+def get_schema(data_set_model):
     schema = {
-        "description": "Schema for {}".format(self.name),
+        "description": "Schema for {}".format(data_set_model.name),
         "definitions": {
             "_timestamp": load_json_schema('timestamp.json'),
         },
         "allOf": [{"$ref": "#/definitions/_timestamp"}]
     }
 
-    data_type = self.data_type.name
-    if data_type in DEFINED_SCHEMAS['data_types']:
+    data_type = data_set_model.data_type.name
+    if data_type in get_defined_schemas('data-types'):
         try:
             schema['definitions'][data_type] = load_json_schema(
-                self.data_type.name)
+                'data-types/'+data_set_model.data_type.name)
 
             schema["allOf"].append(
                 {"$ref": "#/definitions/{}".format(data_type)}
