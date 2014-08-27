9c654cc7bc2fbd5fd2560ed19614c4c7af74c8d4
Tom Booth <tombooth@gmail.com>
2014-08-27 17:55:07 +0100

    We need to invalidate on update as well as reauth
    Signon will send a reauth message when a user is logged out or
    suspended, but a user can also lose permission to our app through their
    permisssions being updated.
    
    We should be invalidating on an update otherwise a user can be updated
    to not have permissions and carry on using the app. BAD!

--- stagecraft/apps/datasets/views/auth.py
+++ stagecraft/apps/datasets/views/auth.py
@@ -1,16 +1,16 @@
 from django.http import HttpResponse
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_http_methods
 
 from ..models.oauth_user import OAuthUser
 from stagecraft.libs.authorization.http import permission_required
 
 
 @csrf_exempt
-@require_POST
+@require_http_methods(['POST', 'PUT'])
 @permission_required('user_update_permission')
 @never_cache
-def reauth(user, request, uid):
+def invalidate(user, request, uid):
     OAuthUser.objects.purge_user(uid)
     return HttpResponse('ok')
