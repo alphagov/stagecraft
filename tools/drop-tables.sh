0dbb6c16a32bae4f3ce4a6f148ff0eae2d14abe8
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-09-03 15:57:06 +0100

    Add script to drop all stagecraft tables
    Drop the entire 'public' schema and then recreate it.

---
+++ tools/drop-tables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo su postgres <<'EOF'
+psql -c 'DROP SCHEMA public;' stagecraft
+psql -c 'CREATE SCHEMA public;' stagecraft
+psql -c 'ALTER SCHEMA public OWNER TO stagecraft;' stagecraft
+EOF
