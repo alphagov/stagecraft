8fafef4c2151d17133c5787847d68ab4b58f40c3
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-09-04 09:12:46 +0100

    Extend JSON serialiser to use serialize() method
    If an object is a UUID, return a string representation of it.
    
    If the object still can't be serialised, call its serialize() method.
    This is useful when nesting Link models inside dashboards, for
    example.

--- stagecraft/libs/views/utils.py
+++ stagecraft/libs/views/utils.py
@@ -2,6 +2,18 @@ import json
 
 from django.utils.cache import patch_response_headers
 from functools import wraps
+from uuid import UUID
+
+
+class JsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, UUID):
+            return '{}'.format(obj)
+
+        if hasattr(obj, 'serialize'):
+            return obj.serialize()
+
+        return json.JSONEncoder.default(self, obj)
 
 
 def long_cache(a_view):
@@ -14,4 +26,4 @@ def long_cache(a_view):
 
 
 def to_json(what):
-    return json.dumps(what, indent=1)
+    return json.dumps(what, indent=1, cls=JsonEncoder)
