4224bfa23c44bf91f1669846165fa5918d25d3b2
Tim Mower <timothy.mower@gmail.com>
2014-09-05 10:42:02 +0100

    Organisations now created and updating

--- stagecraft/apps/dashboards/lib/spotlight_config_migration.py
+++ stagecraft/apps/dashboards/lib/spotlight_config_migration.py
@@ -11,9 +11,10 @@ def spotlight_json(path):
     for root, dirnames, filenames in os.walk(path):
         for filename in fnfilter(filenames, '*.json'):
             with open(os.path.join(root, filename)) as jsonfile:
-                yield json.loads(jsonfile.read())
+                yield (filename, json.loads(jsonfile.read()))
 
 
+logger = logging.getLogger(__name__)
 
 
 class Dashboard():
@@ -21,62 +22,59 @@ class Dashboard():
     def __init__(self, url):
         self.url = url
         self.type_id_map = {}
+        self.stagecraft_client = StagecraftClient(url)
+        self.logger = logging.getLogger(__name__)
 
     def set_data(self, **kwargs):
         self.data = kwargs
 
-    def build_organisation_data(self):
-        return {
-            "name": self.data["department"]["title"],
-            "abbreviation": self.data["department"]["abbr"]
-        }
-
     def get_type_id(self, type_str):
         if type_str not in self.type_id_map:
-            type_request = requests.get(
-                self.url + "/organisation/type",
-                params={"name": type}
-            )
+            type_request = self.stagecraft_client.get_organisation_type(
+                type_str)
             if len(type_request.json()) == 1:
-                self.type_id_map[type_str] = type_request.json()['id']
+                self.type_id_map[type_str] = type_request.json().pop()['id']
             else:
-                logging.getLogger(__name__).info('Unknown type {} for dashboard {}, creating'.format(
-                    type_str, self.data['slug']))
-                post_request = requests.post(
-                    self.url + "/organisation/type",
-                    {"name" :type_str}
+                logger.info(
+                    'Unknown type {} for dashboard {}, creating'.format(
+                        type_str, self.data['slug']))
+                post_response = (
+                    self.stagecraft_client.create_organisation_type(type_str)
                 )
-                self.type_id_map[type_str] = post_request.json()['id']
+                self.type_id_map[type_str] = post_response.json()['id']
 
         return self.type_id_map[type_str]
 
     def create_organisation(self, organisation_type, parent_id=None):
-            resp = requests.get(
-                self.url + "/organisation/node",
-                params={
-                    "name": self.data[organisation_type]["title"],
-                    "abbreviation": self.data[organisation_type]['abbr']
-                }
+            resp = self.stagecraft_client.get_organisation(
+                self.data[organisation_type]['title'],
+                self.data[organisation_type].get('abbr', None)
             )
-            print resp.content
             if resp.status_code == 200 and len(resp.json()) == 0:
                 post_data = {
                     "name": self.data[organisation_type]["title"],
-                    "abbreviation": self.data[organisation_type]["abbr"],
                     "type_id": self.get_type_id(organisation_type)
                 }
+                if "abbr" in self.data[organisation_type]:
+                    post_data["abbreviation"] = (
+                        self.data[organisation_type]["abbr"]
+                    )
                 if parent_id:
                     post_data['parent_id'] = parent_id
-                post_resp = requests.post(
-                    self.url + "/organisation/node", post_data
-                )
-                org_id = post_resp.json()[0]['id']
-            elif len(response.json()) > 1:
-                logger.warning(
+                post_resp = self.stagecraft_client.create_organisation(
+                    post_data)
+                self.logger.debug(
+                    'response from creating org type {} - {}'.format(
+                        organisation_type, post_resp.json()))
+                org_id = post_resp.json()['id']
+            elif len(resp.json()) > 1:
+                self.logger.warning(
                     'multiple organisations found for dashboard{}'.format(
                         self.data['slug']
                     ))
-                org_id = resp.json()['id']
+                org_id = resp.json()[0]['id']
+            else:
+                org_id = resp.json()[0]['id']
             self.data.pop(organisation_type)
             return org_id
 
@@ -87,12 +85,69 @@ class Dashboard():
         if 'department' in self.data:
             department_id = self.create_organisation('department')
         if 'agency' in self.data:
-            agency_id = self.create_organisation('agency', parent_id=department_id)
+            agency_id = self.create_organisation(
+                'agency', parent_id=department_id)
         if agency_id and department_id:
             self.data["organisation"] = agency_id
         elif department_id:
             self.data["organisation"] = department_id
-        requests.post(self.url + "/dashboard/", self.data)
-# stub to use when unit testing
-# with patch('__main__.spotlight_json') as mock:
-#         print i
+        self.stagecraft_client.create_dashboard(self.data)
+
+
+class StagecraftClient():
+
+    def __init__(self, url, token=None):
+        self.url = url
+        if not token:
+            self.token = 'development-oauth-access-token'
+        else:
+            self.token = token
+
+    def create_dashboard(self, data):
+        return self.post("/dashboard", data)
+
+    def create_organisation(self, data):
+        return self.post("/organisation/node", data)
+
+    def create_organisation_type(self, name):
+        return self.post("/organisation/type", {"name": name})
+
+    def post(self, url, data):
+        logger.debug('Posting {} to {}'.format(data, url))
+        headers = {}
+        headers['Authorization'] = 'Bearer {}'.format(self.token)
+        headers['Content-type'] = 'application/json'
+        return requests.post(
+            self.url + url, data=json.dumps(data), headers=headers)
+
+    def get(self, url, params={}):
+        logger.debug('Getting from {}, with params {}'.format(url, params))
+        response = requests.get(
+            self.url + url,
+            params=params
+        )
+        logger.debug('Response: {}: {}'.format(
+            response.status_code, response.content))
+        return response
+
+    def get_organisation(self, name, abbreviation):
+        params = {}
+        if abbreviation:
+            params['abbreviation'] = abbreviation
+            resp = self.get(
+                "/organisation/node",
+                params
+            )
+        else:
+            params['name'] = name
+            resp = self.get(
+                "/organisation/node",
+                params
+            )
+        return resp
+
+    def get_organisation_type(self, name):
+        return self.get(
+            "/organisation/type",
+            params={"name": name}
+        )
--- test_migration.py
+++ test_migration.py
@@ -5,8 +5,17 @@ import os
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-for json in spotlight_json(os.path.join(os.path.dirname(__file__), 'test_import')):
-    dashboard = Dashboard('http://stagecraft.development.performance.service.gov.uk')
-    dashboard.set_data(**json)
-    dashboard.send()
+# for json in spotlight_json(os.path.join(os.path.dirname(__file__), 'test_import')):
+for filename, json in spotlight_json(os.path.join(os.path.dirname(__file__), '../spotlight/app/support/stagecraft_stub/responses')):
+    logger.debug('Creating dashboard for {}'.format(filename))
+    dashboard = Dashboard(
+        'http://stagecraft.development.performance.service.gov.uk')
+    if isinstance(json, dict):
+        dashboard.set_data(**json)
+        dashboard.send()
+    else:
+        logger.warning(
+            'skipping dashboard {} as it is not a valid dictionary'.format(
+                filename))
