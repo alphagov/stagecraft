aa4f2c648acb4df6c38c3b731c254092db1f5065
Tim Mower <timothy.mower@gmail.com>
2014-09-08 12:41:23 +0100

    More explicit error handling

--- test_migration.py
+++ test_migration.py
@@ -7,15 +7,21 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-# for json in spotlight_json(os.path.join(os.path.dirname(__file__), 'test_import')):
+# for filename, json in spotlight_json(os.path.join(os.path.dirname(__file__), 'test_import')):
 for filename, json in spotlight_json(os.path.join(os.path.dirname(__file__), '../spotlight/app/support/stagecraft_stub/responses')):
     logger.debug('Creating dashboard for {}'.format(filename))
     dashboard = Dashboard(
         'http://stagecraft.development.performance.service.gov.uk')
-    if isinstance(json, dict) and json.get('published', None):
-        dashboard.set_data(**json)
-        dashboard.send()
-    else:
+    if not isinstance(json, dict):
         logger.warning(
             'skipping dashboard {} as it is not a valid dictionary'.format(
                 filename))
+    elif json.get('published', None) is None:
+        logger.warning(
+            'skipping dashboard {} as it does not have a published attribute'.format(
+                filename))
+    else:
+        dashboard.set_data(**json)
+        dashboard.send()
+
+print 'All migrated'
