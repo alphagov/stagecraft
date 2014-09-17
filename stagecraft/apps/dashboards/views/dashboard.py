10911482c269155b2d2428067d6bf98ce1ebe532
Tim Mower <timothy.mower@gmail.com>
2014-09-17 12:33:44 +0100

    Move logic from view to model
    Further changes might use the presenter pattern, but for now it is good
    to have all the spotlightify logic in one place.

--- stagecraft/apps/dashboards/views/dashboard.py
+++ stagecraft/apps/dashboards/views/dashboard.py
@@ -27,7 +27,7 @@ def dashboards_for_spotlight(request):
     dashboard = recursively_fetch_dashboard(dashboard_slug)
     if not dashboard:
         return error_response(dashboard_slug)
-    dashboard_json = get_modules_or_tabs(dashboard_slug, dashboard)
+    dashboard_json = dashboard.spotlightify(dashboard_slug)
     if not dashboard_json:
         return error_response(dashboard_slug)
     json_str = to_json(dashboard_json)
@@ -51,50 +51,6 @@ def error_response(dashboard_slug):
     logger.warn(error)
     return HttpResponseNotFound(to_json(error),
                                 content_type='application/json')
-
-
-def get_modules_or_tabs(request_slug, dashboard):
-    # first part will always be empty as we never end the dashboard slug with
-    # a slash
-    dashboard_json = dashboard.spotlightify()
-    remaining_parts = request_slug.replace(dashboard.slug, '').split('/')[1:]
-    if len(remaining_parts) == 0:
-        return dashboard_json
-    if 'modules' not in dashboard_json:
-        return None
-    module = get_single_module_from_dashboard(
-        remaining_parts[0], dashboard_json)
-    if module is None:
-        return None
-    if len(remaining_parts) == 1:
-        dashboard_json['modules'] = [module]
-    elif len(remaining_parts) == 2:
-        tab_slug = remaining_parts[1].replace(remaining_parts[0] + '-', '')
-        tab = get_single_tab_from_module(tab_slug, module)
-        if tab is None:
-            return None
-        tab['info'] = module['info']
-        tab['title'] = module['title'] + ' - ' + tab['title']
-        dashboard_json['modules'] = [tab]
-    else:
-        return None
-    dashboard_json['page-type'] = 'module'
-    return dashboard_json
-
-
-def get_single_module_from_dashboard(module_slug, dashboard_json):
-    return find_first_item_matching_slug(
-        dashboard_json['modules'], module_slug)
-
-
-def get_single_tab_from_module(tab_slug, module_json):
-    return find_first_item_matching_slug(module_json['tabs'], tab_slug)
-
-
-def find_first_item_matching_slug(item_list, slug):
-    for item in item_list:
-        if item['slug'] == slug:
-            return item
 
 
 def recursively_fetch_dashboard(dashboard_slug, count=3):
