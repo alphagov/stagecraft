ea28e24540fef203c9010925390353f00a5a8ac9
Timothy Mower <timothy.mower@gmail.com>
2015-01-15 10:23:32 +0000

    Add pep8 exclusion
    .ropeproject is used by https://github.com/klen/python-mode and creates
    python code which doesnt lint

--- pep-it.sh
+++ pep-it.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 basedir=$(dirname $0)
-pep8 --exclude=migrations,build,venv "$basedir"
+pep8 --exclude=migrations,build,venv,.ropeproject "$basedir"
 pep8 "$basedir/stagecraft/apps/datasets/tests"
 pep8 "$basedir/stagecraft/apps/organisation/tests"
 pep8 "$basedir/stagecraft/apps/transforms/tests"
