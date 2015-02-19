7beffe9f11dd338d2e641c9bb7650a78e0e94e41
Jacob Ashdown <jcbashdown@gmail.com>
2015-02-19 15:40:08 +0000

    Creating nodes in the org graph from built up dict
    - This allows us to use the flat list of dicts representing
      organisations and there parents to be saved to the db.
    - Currently we do no link to dashboards but in this simple test case the
    ancestors are correct.

--- stagecraft/tools/tests/test_load_organisations.py
+++ stagecraft/tools/tests/test_load_organisations.py
@@ -3,14 +3,17 @@ from hamcrest import (
 )
 from mock import patch
 import json
+from stagecraft.apps.organisation.models import Node, NodeType
 
-from stagecraft.apps.dashboards.tests.factories.factories import ModuleFactory
+from stagecraft.apps.dashboards.tests.factories.factories import(
+    ModuleFactory)
 from stagecraft.apps.datasets.tests.factories import DataSetFactory
 
 from ..load_organisations import(
     load_organisations,
     build_up_org_hash,
-    build_up_node_hash)
+    build_up_node_hash,
+    create_nodes)
 
 
 with open('stagecraft/tools/fixtures/result.json', 'r') as f:
@@ -52,35 +55,80 @@ def test_load_organisations(mock_load_data):
         'Department of Administrative Affairs'))
     assert_that(org_parents[3].typeOf.name, is_('department'))
 
+# this is the intermediate data format built up from external data
+# it is then used to actually create and update things in the database.
+expected_result = {
+    'Training and resources on workplace relations: registrations': {
+        'name': "Training and resources on"
+                " workplace relations: registrations",
+        'abbreviation': None,
+        'typeOf': 'transaction',
+        'parents': ['Training and resources on workplace relations']
+    },
+    'Training and resources on workplace relations': {
+        'name': 'Training and resources on workplace relations',
+        'abbreviation': None,
+        'typeOf': 'service',
+        'parents': [u'cps']
+    },
+    u'cps': {
+        'name': u'Crown Prosecution Service',
+        'abbreviation': u'CPS',
+        'typeOf': 'agency',
+        'parents': [u'ago']
+    },
+    u'ago': {
+        'name': "Attorney General's Office",
+        'abbreviation': u'AGO',
+        'typeOf': 'department',
+        'parents': []
+    }
+}
+
+
+def test_create_nodes():
+    create_nodes(expected_result)
+
+    assert_that(len(Node.objects.all()), is_(4))
+
+    transaction = Node.objects.get(
+        name="Training and resources on"
+             " workplace relations: registrations")
+    tx_ancestors = [
+        ancestor.name for ancestor in transaction.get_ancestors()]
+    assert_that(tx_ancestors, equal_to(
+        ["Attorney General's Office",
+         "Crown Prosecution Service",
+         "Training and resources on workplace relations",
+         "Training and resources on workplace relations: registrations"]))
+
+    service = Node.objects.get(
+        name="Training and resources on"
+             " workplace relations")
+    service_ancestors = [
+        ancestor.name for ancestor in service.get_ancestors()]
+    assert_that(service_ancestors, equal_to(
+        ["Attorney General's Office",
+         "Crown Prosecution Service",
+         "Training and resources on workplace relations"]))
+
+    agency = Node.objects.get(
+        name="Crown Prosecution Service")
+    agency_ancestors = [
+        ancestor.name for ancestor in agency.get_ancestors()]
+    assert_that(agency_ancestors, equal_to(
+        ["Attorney General's Office",
+         "Crown Prosecution Service"]))
+
+    department = Node.objects.get(
+        name="Attorney General's Office")
+    department_ancestors = [
+        ancestor.name for ancestor in department.get_ancestors()]
+    assert_that(department_ancestors, equal_to(
+        ["Attorney General's Office"]))
+
 
 def test_build_up_node_hash():
-    expected_result = {
-        'Training and resources on workplace relations: registrations': {
-            'name': "Training and resources on"
-                    " workplace relations: registrations",
-            'abbreviation': '',
-            'typeOf': 'transaction',
-            'parents': ['Training and resources on workplace relations']
-        },
-        'Training and resources on workplace relations': {
-            'name': 'Training and resources on workplace relations',
-            'abbreviation': '',
-            'typeOf': 'service',
-            'parents': [u'cps']
-        },
-        u'cps': {
-            'name': u'Crown Prosecution Service',
-            'abbreviation': u'CPS',
-            'typeOf': 'agency',
-            'parents': [u'ago']
-        },
-        u'ago': {
-            'name': "Attorney General's Office",
-            'abbreviation': u'AGO',
-            'typeOf': 'department',
-            'parents': []
-        }
-    }
     result = build_up_node_hash(tx_fixture, govuk_fixture)
     assert_that(result, equal_to(expected_result))
 
