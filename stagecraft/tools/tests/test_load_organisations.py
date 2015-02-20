37702a71e15fb638c5e4e944a950912dc980042e
Jacob Ashdown <jcbashdown@gmail.com>
2015-02-20 12:30:47 +0000

    Tidy up, make more robust, better logging
    This will now return a dict of what was done so that we can discern
    changes due to changes in the source data and also whether the counts
    look sensible - are they what we expected? Are there areas we must
    address?
    
    - overwrite innaccurate typof with txex info.
    - defend against '' or None parents
    - catch and log data and integrity errors
    - clear all existing orgs - we don't care about non transaction
      dashboards for now.

--- stagecraft/tools/tests/test_load_organisations.py
+++ stagecraft/tools/tests/test_load_organisations.py
@@ -1,6 +1,7 @@
 from hamcrest import (
     assert_that, is_not, is_, equal_to
 )
+from django.test import TestCase
 from mock import patch
 import json
 from stagecraft.apps.organisation.models import Node
@@ -25,72 +26,162 @@ with open('stagecraft/tools/fixtures/organisations.json', 'r') as f:
     govuk_fixture = json.loads(f.read())
 
 
-@patch('stagecraft.tools.load_organisations.load_data')
-def test_load_organisations(mock_load_data):
-    mock_load_data.return_value = tx_fixture, govuk_fixture
+class LoadOrganisationsTestCase(TestCase):
 
-    tx_data_set = DataSetFactory(
-        data_group__name='transactional_services',
-        data_type__name='summaries'
-    )
-    kpi_module = ModuleFactory(
-        dashboard=DashboardFactory(published=True),
-        data_set=tx_data_set,
-        query_parameters={
-            'filter_by': ['service_id:bis-acas-elearning-registrations'],
-        }
-    )
-    dashboard = kpi_module.dashboard
+    @classmethod
+    def setUpClass(cls):
+        cls.tx_data_set = DataSetFactory(
+            data_group__name='transactional_services',
+            data_type__name='summaries'
+        )
+        cls.kpi_module = ModuleFactory(
+            dashboard=DashboardFactory(published=True),
+            data_set=cls.tx_data_set,
+            query_parameters={
+                'filter_by': ['service_id:bis-acas-elearning-registrations'],
+            }
+        )
 
-    load_organisations('foo', 'bar')
+    @patch('stagecraft.tools.load_organisations.load_data')
+    def test_load_organisations(self, mock_load_data):
+        mock_load_data.return_value = tx_fixture, govuk_fixture
 
-    dashboard = Dashboard.objects.get(id=dashboard.id)
+        dashboard = self.__class__.kpi_module.dashboard
 
-    assert_that(dashboard.organisation, is_not(None))
+        load_organisations('foo', 'bar')
 
-    org_parents = [org for org in dashboard.organisation.get_ancestors(
-        include_self=True)]
+        dashboard = Dashboard.objects.get(id=dashboard.id)
 
-    assert_that(len(org_parents), is_(4))
+        assert_that(dashboard.organisation, is_not(None))
 
-    assert_that(
-        dashboard.organisation.name,
-        equal_to(
-            "Training and resources on workplace relations: registrations"))
-    assert_that(
-        dashboard.organisation.typeOf.name,
-        equal_to("transaction"))
+        org_parents = [org for org in dashboard.organisation.get_ancestors(
+            include_self=True)]
 
-    parent_org = dashboard.organisation.parents.first()
-    assert_that(
-        parent_org.name,
-        equal_to("Training and resources on workplace relations"))
-    assert_that(
-        parent_org.typeOf.name,
-        equal_to("service"))
+        assert_that(len(org_parents), is_(5))
 
-    parent_org = parent_org.parents.first()
-    assert_that(
-        parent_org.name,
-        equal_to("Crown Prosecution Service"))
-    assert_that(
-        parent_org.typeOf.name,
-        equal_to("agency"))
+        assert_that(
+            dashboard.organisation.name,
+            equal_to(
+                "Registrations to use online training and resources on"
+                " workplace relations"))
+        assert_that(
+            dashboard.organisation.typeOf.name,
+            equal_to("transaction"))
 
-    parent_org = parent_org.parents.first()
-    assert_that(
-        parent_org.name,
-        equal_to("Attorney General's Office"))
-    assert_that(
-        parent_org.typeOf.name,
-        equal_to("department"))
+        parent_org = dashboard.organisation.parents.first()
+        assert_that(
+            parent_org.name,
+            equal_to("Training and resources on workplace relations"))
+        assert_that(
+            parent_org.typeOf.name,
+            equal_to("service"))
+
+        parent_org = parent_org.parents.first()
+        assert_that(
+            parent_org.name,
+            equal_to("Crown Prosecution Service"))
+        assert_that(
+            parent_org.typeOf.name,
+            equal_to("agency"))
+
+        parents = parent_org.parents.all()
+        assert_that(len(parents), equal_to(2))
+        parent_org = parents[0]
+        assert_that(
+            parent_org.name,
+            equal_to("Attorney General's Office"))
+        assert_that(
+            parent_org.typeOf.name,
+            equal_to("department"))
+        parent_org = parents[1]
+        assert_that(
+            parent_org.name,
+            equal_to("Foo thing"))
+        assert_that(
+            parent_org.typeOf.name,
+            equal_to("department"))
+
+    @patch('stagecraft.tools.load_organisations.load_data')
+    def test_load_organisations_same_result_if_run_twice(self, mock_load_data):
+        mock_load_data.return_value = tx_fixture, govuk_fixture
+
+        what_happened = load_organisations('foo', 'bar')
+        assert_that(len(what_happened['dashboards_at_start']), equal_to(1))
+        assert_that(len(what_happened['dashboards_at_end']), equal_to(1))
+        assert_that(len(what_happened['total_nodes_before']), equal_to(0))
+        assert_that(len(what_happened['total_nodes_after']), equal_to(5))
+        assert_that(len(what_happened['organisations']), equal_to(3))
+        assert_that(len(what_happened['transactions']), equal_to(1))
+        assert_that(len(what_happened['created_nodes']), equal_to(5))
+        assert_that(len(what_happened['existing_nodes']), equal_to(0))
+        assert_that(
+            len(what_happened['unable_to_find_or_create_nodes']), equal_to(0))
+        assert_that(
+            len(what_happened['unable_existing_nodes_diff_details']),
+            equal_to(0))
+        assert_that(
+            len(what_happened['unable_existing_nodes_diff_details_msgs']),
+            equal_to(0))
+        assert_that(len(what_happened['unable_data_error_nodes']), equal_to(0))
+        assert_that(
+            len(what_happened['unable_data_error_nodes_msgs']), equal_to(0))
+        assert_that(len(what_happened['duplicate_services']), equal_to(0))
+        assert_that(len(what_happened['duplicate_transactions']), equal_to(0))
+        assert_that(len(
+            what_happened['duplicate_dep_or_agency_abbreviations']),
+            equal_to(0))
+        assert_that(
+            len(what_happened['link_to_parents_not_found']),
+            equal_to(0))
+        assert_that(len(what_happened['link_to_parents_found']), equal_to(1))
+        assert_that(
+            len(what_happened['transactions_associated_with_dashboards']),
+            equal_to(1))
+        assert_that(
+            len(what_happened['transactions_not_associated_with_dashboards']),
+            equal_to(0))
+
+        what_happened = load_organisations('foo', 'bar')
+        assert_that(len(what_happened['dashboards_at_start']), equal_to(1))
+        assert_that(len(what_happened['dashboards_at_end']), equal_to(1))
+        assert_that(len(what_happened['total_nodes_before']), equal_to(0))
+        assert_that(len(what_happened['total_nodes_after']), equal_to(5))
+        assert_that(len(what_happened['organisations']), equal_to(3))
+        assert_that(len(what_happened['transactions']), equal_to(1))
+        assert_that(len(what_happened['created_nodes']), equal_to(5))
+        assert_that(len(what_happened['existing_nodes']), equal_to(0))
+        assert_that(
+            len(what_happened['unable_to_find_or_create_nodes']), equal_to(0))
+        assert_that(
+            len(what_happened['unable_existing_nodes_diff_details_msgs']),
+            equal_to(0))
+        assert_that(len(what_happened['unable_data_error_nodes']), equal_to(0))
+        assert_that(
+            len(what_happened['unable_data_error_nodes_msgs']), equal_to(0))
+        assert_that(len(what_happened['unable_data_error_nodes']), equal_to(0))
+        assert_that(len(what_happened['duplicate_services']), equal_to(0))
+        assert_that(len(what_happened['duplicate_transactions']), equal_to(0))
+        assert_that(len(
+            what_happened['duplicate_dep_or_agency_abbreviations']),
+            equal_to(0))
+        assert_that(
+            len(what_happened['link_to_parents_not_found']),
+            equal_to(0))
+        assert_that(len(what_happened['link_to_parents_found']), equal_to(1))
+        assert_that(
+            len(what_happened['transactions_associated_with_dashboards']),
+            equal_to(1))
+        assert_that(
+            len(what_happened['transactions_not_associated_with_dashboards']),
+            equal_to(0))
 
 # this is the intermediate data format built up from external data
 # it is then used to actually create and update things in the database.
 expected_result = {
-    'Training and resources on workplace relations: registrations': {
-        'name': "Training and resources on"
-                " workplace relations: registrations",
+    "Registrations to use online training and resources"
+    " on workplace relations": {
+        'name': "Registrations to use online training and resources"
+                " on workplace relations",
         'abbreviation': None,
         'typeOf': 'transaction',
         'parents': ['Training and resources on workplace relations']
@@ -105,11 +196,17 @@ expected_result = {
         'name': u'Crown Prosecution Service',
         'abbreviation': u'CPS',
         'typeOf': 'agency',
-        'parents': [u'ago']
+        'parents': [u'ago', u'foo']
     },
     u'ago': {
         'name': "Attorney General's Office",
         'abbreviation': u'AGO',
+        'typeOf': 'department',
+        'parents': []
+    },
+    u'foo': {
+        'name': u'Foo thing',
+        'abbreviation': u'foo',
         'typeOf': 'department',
         'parents': []
     }
@@ -119,18 +216,20 @@ expected_result = {
 def test_create_nodes():
     create_nodes(expected_result)
 
-    assert_that(len(Node.objects.all()), is_(4))
+    assert_that(len(Node.objects.all()), is_(5))
 
     transaction = Node.objects.get(
-        name="Training and resources on"
-             " workplace relations: registrations")
+        name="Registrations to use online training and resources on"
+             " workplace relations")
     tx_ancestors = [
         ancestor.name for ancestor in transaction.get_ancestors()]
     assert_that(tx_ancestors, equal_to(
         ["Attorney General's Office",
+         "Foo thing",
          "Crown Prosecution Service",
          "Training and resources on workplace relations",
-         "Training and resources on workplace relations: registrations"]))
+         "Registrations to use online training and resources on"
+         " workplace relations"]))
 
     service = Node.objects.get(
         name="Training and resources on"
@@ -139,6 +238,7 @@ def test_create_nodes():
         ancestor.name for ancestor in service.get_ancestors()]
     assert_that(service_ancestors, equal_to(
         ["Attorney General's Office",
+         "Foo thing",
          "Crown Prosecution Service",
          "Training and resources on workplace relations"]))
 
@@ -148,6 +248,7 @@ def test_create_nodes():
         ancestor.name for ancestor in agency.get_ancestors()]
     assert_that(agency_ancestors, equal_to(
         ["Attorney General's Office",
+         "Foo thing",
          "Crown Prosecution Service"]))
 
     department = Node.objects.get(
@@ -168,12 +269,18 @@ def test_build_up_org_hash():
         'cps': {
             'name': 'Crown Prosecution Service',
             'abbreviation': 'CPS',
-            'typeOf': 'agency',
-            'parents': ['ago']
+            'typeOf': 'department',
+            'parents': ['ago', 'foo']
         },
         'ago': {
             'name': "Attorney General's Office",
             'abbreviation': 'AGO',
+            'typeOf': 'department',
+            'parents': []
+        },
+        'foo': {
+            'name': "Foo thing",
+            'abbreviation': 'foo',
             'typeOf': 'department',
             'parents': []
         }
