fe4be96cc42e9fe7969389f12e3d83094f3f7aa7
Matt Cottingham <matt.cottingham@digital.cabinet-office.gov.uk>
2015-03-04 13:46:07 +0000

    Update spreadsheet merging and simplify tests
    This updates the spreadsheet merging code to
    use the new format.
    
    It also allows updating of existing dashboards
    based on the fields in the spreadsheets.

--- stagecraft/tools/spreadsheets.py
+++ stagecraft/tools/spreadsheets.py
@@ -14,8 +14,8 @@ REPLACE_TABLE = {
 class SpreadsheetMunger:
 
     def __init__(self, positions={}):
-        # The transaction explorer spreadsheet is less likely to change.
-        # As a result we can assume these positions.
+        # The transaction explorer spreadsheet is less likely
+        # to change so we can assume these positions.
         self.tx_name = positions.get('tx_name', 4)
         self.tx_desc1 = positions.get('tx_desc1', 65)
         self.tx_desc2 = positions.get('tx_desc2', 66)
@@ -26,15 +26,14 @@ class SpreadsheetMunger:
         self.tx_high_volume = positions.get('tx_high_volume', 74)
         self.tx_costs = positions.get('tx_costs', 68)
         self.tx_other_notes = positions.get('tx_other_notes', 69)
-
         self.tx_tx_id_column = positions.get('tx_tx_id_column', 6)
 
+        self.names_description = positions['names_description']
         self.names_name = positions['names_name']
         self.names_slug = positions['names_slug']
-        self.names_service_name = positions['names_service_name']
-        self.names_service_slug = positions['names_service_slug']
-
-        self.names_tx_id_column = positions['names_tx_id_column']
+        self.names_notes = positions['names_notes']
+        self.names_other_notes = positions['names_other_notes']
+        self.names_tx_id = positions['names_tx_id']
 
     def _replace_unicode(self, s):
         for k, v in REPLACE_TABLE.items():
@@ -42,29 +41,12 @@ class SpreadsheetMunger:
         return s
 
     def _parse_tx_row(self, row):
-        name = self._replace_unicode(row[self.tx_name])
-        if len(name) > 80:
-            name = name[:80]
-
-        description = self._replace_unicode(row[self.tx_desc1])
-        if len(description) > 500:
-            description = description[:500]
-
-        description_extra = self._replace_unicode(row[self.tx_desc2])
-        if len(description_extra) > 400:
-            description_extra = description_extra[:400]
-
-        costs = self._replace_unicode(row[self.tx_costs])
-        if len(costs) > 1500:
-            costs = costs[:1500]
-
-        other_notes = self._replace_unicode(row[self.tx_other_notes])
-        if len(other_notes) > 1000:
-            other_notes = other_notes[:1000]
-
-        slug = row[self.tx_tx_id_column]
-        if len(slug) > 90:
-            slug = slug[:90]
+        name = row[self.tx_name]
+        description = row[self.tx_desc1]
+        description_extra = row[self.tx_desc2]
+        costs = row[self.tx_costs]
+        other_notes = row[self.tx_other_notes]
+        tx_id = row[self.tx_tx_id_column]
 
         agency = {
             'abbr': row[self.tx_agency_abbr],
@@ -86,10 +68,10 @@ class SpreadsheetMunger:
             high_volume = True
 
         return {
+            'tx_id': tx_id,
             'name': name,
             'description': description,
             'description_extra': description_extra,
-            'slug': slug,
             'department': department,
             'agency': agency,
             'high_volume': high_volume,
@@ -98,35 +80,20 @@ class SpreadsheetMunger:
         }
 
     def _parse_names_row(self, row):
+
         return {
+            'tx_id': row[self.names_tx_id],
             'name': row[self.names_name],
-            'slug': row[self.names_slug][1:],  # cut leading slash
-            'service': {
-                'name': row[self.names_service_name],
-                'slug': row[self.names_service_slug][1:],  # cut leading slash
-            }
+            'description': row[self.names_description],
+            # description-extra is not in the new spreadsheet
+            'slug': row[self.names_slug][1:], # cut leading slash
+            'costs': row[self.names_notes],
+            'other_notes': row[self.names_other_notes],
         }
 
     def _parse_rows(self, worksheet, parse_fn, tx_id_column):
         return {r[tx_id_column]: parse_fn(r)
                 for r in worksheet.get_all_values()[1:]}
-
-    def _merge(self, tx, names):
-        merged = []
-
-        for tx_id, tx_datum in tx.items():
-            try:
-                names_datum = names[tx_id]
-                merged.append(
-                    dict(list(tx_datum.items()) +
-                         list(names_datum.items()) +
-                         [('tx_id', tx_id)]))
-            except KeyError:
-                print(
-                    'Failed to find names spreadsheet record for {}'\
-                            .format(tx_id))
-
-        return merged
 
     def load_tx_worksheet(self, username, password):
         account = gspread.login(username, password)
@@ -168,6 +135,67 @@ class SpreadsheetMunger:
             rows.append(self._parse_names_row(row))
         return rows
 
+    def _merge(self, tx, names):
+        # Records are supplied as rows, so convert to dict keyed by tx_id.
+        tx_dict = {}
+        for record in tx:
+            tx_dict[record['tx_id']] = record
+        names_dict = {}
+        for record in names:
+            names_dict[record['tx_id']] = record
+
+        merged = []
+        unmerged = []
+        for tx_id, tx_datum in tx_dict.items():
+            try:
+                names_datum = names_dict[tx_id]
+                merged.append(
+                    dict(list(tx_datum.items()) +
+                         list(names_datum.items()) +
+                         [('tx_id', tx_id)]))
+            except KeyError:
+                unmerged.append(tx_id)
+        if unmerged:
+            print("There were unmerged records: {}".format(unmerged))
+        return merged
+
+    def sanitise_record(self, record):
+
+        if record.get('name'):
+            name = self._replace_unicode(record['name'])
+            if len(name) > 80:
+                name = name[:80]
+            record['name'] = name
+
+        if record.get('description'):
+            description = self._replace_unicode(record['description'])
+            if len(description) > 500:
+                description = description[:500]
+            record['description'] = description
+
+        if record.get('description_extra'):
+            description_extra = self._replace_unicode(record['description_extra'])
+            if len(description_extra) > 400:
+                description_extra = description_extra[:400]
+            record['description_extra'] = description_extra
+
+        if record.get('costs'):
+            costs = self._replace_unicode(record['costs'])
+            if len(costs) > 1500:
+                costs = costs[:1500]
+            record['costs'] = costs
+
+        if record.get('other_notes'):
+            other_notes = self._replace_unicode(record['other_notes'])
+            if len(other_notes) > 1000:
+                other_notes = other_notes[:1000]
+            record['other_notes'] = other_notes
+
+        if record.get('tx_id'):
+            tx_id = self._replace_unicode(record['tx_id'])
+            if len(tx_id) > 90:
+                tx_id = tx_id[:90]
+            record['tx_id'] = tx_id
 
     def load(self, username, password):
         tx = self.load_tx_worksheet(username, password)
--- stagecraft/tools/test_spreadsheet.py
+++ stagecraft/tools/test_spreadsheet.py
@@ -1,7 +1,4 @@
-import json
-
 from mock import patch
-from nose.tools import nottest
 from hamcrest import (
     assert_that, equal_to
 )
@@ -9,28 +6,48 @@ from hamcrest import (
 from .spreadsheets import SpreadsheetMunger
 
 
-with open('stagecraft/tools/fixtures/tx.json') as f:
-    tx_worksheet = json.loads(f.read())
+TX_RECORDS = [
+    {
+        'agency': {
+            'abbr': 'CPS',
+            'name': 'Crown Prosecution Service',
+            'slug': 'cps'
+        },
+        'costs': '',
+        'department': {
+            'abbr': 'AGO',
+            'name': "Attorney General's Office",
+            'slug': 'ago'
+        },
+        'description': '',
+        'description_extra': '',
+        'high_volume': False,
+        'name': 'Witness expenses: reimbursements',
+        'other_notes': '',
+        'tx_id': 'ago-reimbursing-witness-expenses'
+    }
+]
 
-with open('stagecraft/tools/fixtures/names.json') as f:
-    names_worksheet = json.loads(f.read())
+NAMES_RECORDS = [
+    {
+        'name': 'Court actions',
+        'description': 'description',
+        'slug': 'court-actions-',
+        'tx_id': 'ago-reimbursing-witness-expenses'
+    }
+]
 
-
-@nottest
-@patch('gspread.login')
-def test_load(mock_login):
-    def get_appropriate_spreadsheet():
-        return [tx_worksheet, names_worksheet]
-
-    mock_login().open_by_key().worksheet().get_all_values.side_effect = get_appropriate_spreadsheet()  # noqa
-    munger = SpreadsheetMunger(positions={
-        'names_name': 6,
-        'names_slug': 7,
-        'names_service_name': 4,
-        'names_service_slug': 5,
-        'names_tx_id_column': 16
+def test_merge():
+    loader = SpreadsheetMunger({
+        'names_description': 8,
+        'names_name': 11,
+        'names_slug': 12,
+        'names_notes': 17,
+        'names_other_notes': 18,
+        'names_tx_id': 19,
     })
-    result = munger.load('beep', 'boop')
-    result_path = 'stagecraft/tools/fixtures/spreadsheet_munging_result.json'
-    with open(result_path, 'r') as f:
-        assert_that(json.loads(f.read()), equal_to(result))
+    merged = loader._merge(TX_RECORDS, NAMES_RECORDS)
+    assert_that(merged[0]['tx_id'], 'ago-reimbursing-witness-expenses')
+    assert_that(merged[0]['name'], 'Court actions')
+    assert_that(merged[0]['description'], 'description')
+
