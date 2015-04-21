a3bb5ca86cbba530718e55f97407d7c5d3ad0a57
Tom Booth <tombooth@gmail.com>
2015-04-21 11:25:08 +0100

    Improve Django Admin interface for Organisations
    We have a lot of nodes now so adding some way of filtering by type and
    searching by name seemed wise.
    
    I've switched the multiselect box to pick parents for an interface that
    is a little more sane when we have over a 1000 possible parents.

--- stagecraft/apps/organisation/admin.py
+++ stagecraft/apps/organisation/admin.py
@@ -7,8 +7,20 @@ class NodeTypeAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 
+class ParentInline(admin.TabularInline):
+    model = Node.parents.through
+    verbose_name = 'Parent relationship'
+    verbose_name_plural = 'Parents'
+    extra = 1
+    fk_name = 'from_node'
+
+
 class NodeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'abbreviation',)
+    list_display = ('name', 'abbreviation', 'typeOf',)
+    list_filter = ('typeOf',)
+    search_fields = ('name', 'abbreviation',)
+    inlines = (ParentInline,)
+    exclude = ('parents',)
 
 
 admin.site.register(NodeType, NodeTypeAdmin)
