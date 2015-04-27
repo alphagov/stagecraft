e65aa7620574e178fbdae82376a5188e2f697ae0
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2015-04-27 14:45:33 +0100

    Include Agencies even when missing an abbreviation.
    If we've had issues with Agencies that don't have an abbreviation missing from the org graph.
    This means that we have been mis-representing the structure of government.
    
    If an abbreviation doesn't exist for an agency, still record the agency details against
    the department. Set the slug to the agency name.

--- stagecraft/tools/tests/test_spreadsheet.py
+++ stagecraft/tools/tests/test_spreadsheet.py
@@ -41,3 +41,36 @@ def test_merge():
     result_path = 'stagecraft/tools/fixtures/spreadsheet_munging_result.json'
     with open(result_path, 'r') as f:
         assert_that(result[0], has_entries(json.loads(f.read())[0]))
+
+
+def test_no_agency_abbr():
+
+    with open('stagecraft/tools/fixtures/tx_no_agency_abbr.json') as f:
+        tx_no_abbr_ws = json.loads(f.read())
+
+    munger = SpreadsheetMunger({
+        'names_transaction_name': 6,
+        'names_transaction_slug': 7,
+        'names_service_name': 4,
+        'names_service_slug': 5,
+        'names_tx_id': 16,
+        'names_description': 4,
+        'names_notes': 4,
+        'names_other_notes': 4
+    })
+
+    mock_account = Mock()
+    mock_account.open_by_key().worksheet(
+    ).get_all_values.return_value = tx_no_abbr_ws
+    tx = munger.load_tx_worksheet(mock_account)
+
+    mock_account = Mock()
+    mock_account.open_by_key().worksheet(
+    ).get_all_values.return_value = names_worksheet
+    names = munger.load_names_worksheet(mock_account)
+
+    result = munger.merge(tx, names)
+    result_path = 'stagecraft/tools/fixtures/' \
+                  'spreadsheet_munging_no_agency_abbr_result.json'
+    with open(result_path, 'r') as f:
+        assert_that(result[0], has_entries(json.loads(f.read())[0]))
