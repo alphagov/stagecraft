ea9205460dc0153f027ca52ea9743173427c3a95
Lee Longmore <lee.longmore@gmail.com>
2015-05-07 16:09:35 +0100

    Move users view

--- stagecraft/apps/users/views.py
+++ stagecraft/apps/users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.http import (HttpResponse, HttpResponseNotFound)
+from django.views.decorators.cache import never_cache
+from django.views.decorators.vary import vary_on_headers
+
+from stagecraft.apps.users.models import User
+from stagecraft.libs.authorization.http import permission_required
+from stagecraft.libs.views.utils import to_json, create_error
+
+logger = logging.getLogger(__name__)
+
+
+@permission_required('user')
+@never_cache
+@vary_on_headers('Authorization')
+def detail(authorised_user, request, email):
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        error = {
+            'status': 'error',
+            'message': "No user with email address '{}' exists".format(email)
+        }
+        logger.warn(error)
+
+        error["errors"] = [
+            create_error(request, 404, detail=error['message'])
+        ]
+
+        return HttpResponseNotFound(to_json(error))
+
+    json_str = to_json(user.serialize())
+
+    return HttpResponse(json_str, content_type='application/json')
