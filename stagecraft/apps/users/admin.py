10e7c4d5d4942369110a60b78665dc54460a2285
Lee Longmore <lee.longmore@gmail.com>
2015-05-07 15:49:44 +0100

    Use new user model

--- stagecraft/apps/users/admin.py
+++ stagecraft/apps/users/admin.py
@@ -1,3 +1,32 @@
+from __future__ import unicode_literals
 from django.contrib import admin
+from django.db import models
+import reversion
+from stagecraft.apps.users.models import User
+from stagecraft.apps.datasets.models.data_set import DataSet
 
-# Register your models here.
+
+class DataSetInline(admin.StackedInline):
+    model = DataSet
+    fields = ('name',)
+    extra = 0
+
+
+class UserAdmin(reversion.VersionAdmin):
+    search_fields = ['email']
+    list_display = ('email', 'number_of_datasets_user_has_access_to',)
+    list_per_page = 30
+    filter_horizontal = ('data_sets',)
+
+    def queryset(self, request):
+        return User.objects.annotate(
+            dataset_count=models.Count('data_sets')
+        )
+
+    def number_of_datasets_user_has_access_to(self, obj):
+        return obj.dataset_count
+
+    number_of_datasets_user_has_access_to.admin_order_field = 'dataset_count'
+
+
+admin.site.register(User, UserAdmin)
--- stagecraft/apps/users/tests.py
+++ stagecraft/apps/users/tests.py
@@ -1,3 +1,35 @@
+# encoding: utf-8
+# See https://docs.djangoproject.com/en/1.6/topics/testing/tools/
+
+from __future__ import unicode_literals
+
 from django.test import TestCase
 
-# Create your tests here.
+from django.core.exceptions import ValidationError
+
+from nose.tools import assert_raises, assert_equal
+
+from stagecraft.apps.datasets.models import DataSet
+from stagecraft.apps.users.models import User
+
+
+class UserTestCase(TestCase):
+    fixtures = ['test_import_users_datasets.json']
+
+    def test_user_email_must_be_unique(self):
+        a = User.objects.create(email='email@email.com')
+        a.validate_unique()
+
+        b = User(email='email@email.com')
+        assert_raises(ValidationError, lambda: b.validate_unique())
+
+    def test_serialize_returns_serialized_user(self):
+        a = User.objects.create(email='email@blah.net')
+        a.data_sets.add(DataSet.objects.get(name="evl_customer_satisfaction"))
+        a.data_sets.add(DataSet.objects.get(name="lpa_volumes"))
+        expected_response = {
+            'email': 'email@blah.net',
+            'data_sets': ['evl_customer_satisfaction', 'lpa_volumes']
+        }
+
+        assert_equal(a.serialize(), expected_response)
