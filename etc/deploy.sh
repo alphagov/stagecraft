8218885d3e9644b4581526bc5dd33a424666cfb2
Daniel Blair <daniel.blair@digital.cabinet-office.gov.uk>
2018-08-21 14:30:42 +0100

    "Improved" the deployment so it doesn't always fail.

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+function cycle_app() {
+    local app_name=$1
+
+    # ensure we don't get caught out by caching problems
+    # caused by the same app code having been pushed under a different
+    # app name in the same space (for testing purposes, perhaps).
+    cf delete -f ${app_name}
+    cf push ${app_name} --no-start
+}
 if [ -z "$1" ]; then
     echo "Missing PAAS space argument"
     echo "  deploy.sh staging|production"
@@ -15,6 +24,7 @@ sudo apt-get update && sudo apt-get install cf-cli
 
 cf login -u $PAAS_USER -p $PAAS_PASSWORD -a https://api.cloud.service.gov.uk -o gds-performance-platform -s $PAAS_SPACE
 
+cycle_app performance-platform-stagecraft-web
 # set environmental variables
 cf set-env performance-platform-stagecraft-web SECRET_KEY $APP_SECRET_KEY
 cf set-env performance-platform-stagecraft-web FERNET_KEY $APP_FERNET_KEY
@@ -27,7 +37,11 @@ cf set-env performance-platform-stagecraft-web REDIS_DATABASE_NUMBER $REDIS_DATA
 cf set-env performance-platform-stagecraft-web STAGECRAFT_COLLECTION_ENDPOINT_TOKEN $STAGECRAFT_COLLECTION_ENDPOINT_TOKEN
 cf set-env performance-platform-stagecraft-web TRANSFORMED_DATA_SET_TOKEN $TRANSFORMED_DATA_SET_TOKEN
 cf set-env performance-platform-stagecraft-web MIGRATION_SIGNON_TOKEN $MIGRATION_SIGNON_TOKEN
+cf set-env performance-platform-stagecraft-web DJANGO_SETTINGS_MODULE "stagecraft.settings.$PAAS_SPACE"
+# now rerun with the environment variables
+cf start performance-platform-stagecraft-web
 
+cycle_app performance-platform-stagecraft-celery-worker
 cf set-env performance-platform-stagecraft-celery-worker SECRET_KEY $APP_SECRET_KEY
 cf set-env performance-platform-stagecraft-celery-worker FERNET_KEY $APP_FERNET_KEY
 cf set-env performance-platform-stagecraft-celery-worker SIGNON_CLIENT_ID $SIGNON_CLIENT_ID
@@ -35,7 +49,10 @@ cf set-env performance-platform-stagecraft-celery-worker GOVUK_APP_DOMAIN $GOVUK
 cf set-env performance-platform-stagecraft-celery-worker GOVUK_WEBSITE_ROOT $GOVUK_WEBSITE_ROOT
 cf set-env performance-platform-stagecraft-celery-worker BACKDROP_PUBLIC_DOMAIN $BACKDROP_PUBLIC_DOMAIN
 cf set-env performance-platform-stagecraft-celery-worker REDIS_DATABASE_NUMBER $REDIS_DATABASE_NUMBER
+cf set-env performance-platform-stagecraft-celery-worker DJANGO_SETTINGS_MODULE "stagecraft.settings.$PAAS_SPACE"
+cf start performance-platform-stagecraft-celery-worker
 
+cycle_app performance-platform-stagecraft-celery-beat
 cf set-env performance-platform-stagecraft-celery-beat SECRET_KEY $APP_SECRET_KEY
 cf set-env performance-platform-stagecraft-celery-beat FERNET_KEY $APP_FERNET_KEY
 cf set-env performance-platform-stagecraft-celery-beat SIGNON_CLIENT_ID $SIGNON_CLIENT_ID
@@ -43,7 +60,10 @@ cf set-env performance-platform-stagecraft-celery-beat GOVUK_APP_DOMAIN $GOVUK_A
 cf set-env performance-platform-stagecraft-celery-beat GOVUK_WEBSITE_ROOT $GOVUK_WEBSITE_ROOT
 cf set-env performance-platform-stagecraft-celery-beat BACKDROP_PUBLIC_DOMAIN $BACKDROP_PUBLIC_DOMAIN
 cf set-env performance-platform-stagecraft-celery-beat REDIS_DATABASE_NUMBER $REDIS_DATABASE_NUMBER
+cf set-env performance-platform-stagecraft-celery-beat DJANGO_SETTINGS_MODULE "stagecraft.settings.$PAAS_SPACE"
+cf start performance-platform-stagecraft-celery-beat
 
+cycle_app performance-platform-stagecraft-celery-cam
 cf set-env performance-platform-stagecraft-celery-cam SECRET_KEY $APP_SECRET_KEY
 cf set-env performance-platform-stagecraft-celery-cam FERNET_KEY $APP_FERNET_KEY
 cf set-env performance-platform-stagecraft-celery-cam SIGNON_CLIENT_ID $SIGNON_CLIENT_ID
@@ -51,17 +71,13 @@ cf set-env performance-platform-stagecraft-celery-cam GOVUK_APP_DOMAIN $GOVUK_AP
 cf set-env performance-platform-stagecraft-celery-cam GOVUK_WEBSITE_ROOT $GOVUK_WEBSITE_ROOT
 cf set-env performance-platform-stagecraft-celery-cam BACKDROP_PUBLIC_DOMAIN $BACKDROP_PUBLIC_DOMAIN
 cf set-env performance-platform-stagecraft-celery-cam REDIS_DATABASE_NUMBER $REDIS_DATABASE_NUMBER
-
 cf set-env performance-platform-stagecraft-celery-cam DJANGO_SETTINGS_MODULE "stagecraft.settings.$PAAS_SPACE"
-cf set-env performance-platform-stagecraft-celery-beat DJANGO_SETTINGS_MODULE "stagecraft.settings.$PAAS_SPACE"
-cf set-env performance-platform-stagecraft-celery-worker DJANGO_SETTINGS_MODULE "stagecraft.settings.$PAAS_SPACE"
-cf set-env performance-platform-stagecraft-web DJANGO_SETTINGS_MODULE "stagecraft.settings.$PAAS_SPACE"
+cf start performance-platform-stagecraft-celery-cam
 
+cycle_app performance-platform-stagecraft-flower
 cf set-env performance-platform-stagecraft-flower REDIS_DATABASE_NUMBER $REDIS_DATABASE_NUMBER
 cf set-env performance-platform-stagecraft-celery-worker SECRET_KEY $APP_SECRET_KEY
-
-# deploy apps
-cf push -f manifest.yml
+cf start performance-platform-stagecraft-flower
 
 # create and map routes
 cf map-route performance-platform-stagecraft-web cloudapps.digital --hostname performance-platform-stagecraft-$PAAS_SPACE
